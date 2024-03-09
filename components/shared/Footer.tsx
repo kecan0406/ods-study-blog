b4387bf7769ffdbbb1c5afea77fae81f9d7a681c
kecan0406 <kecan0406@gmail.com>
2024-03-10 01:31:48 +0900

    ✨ Footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,25 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 const Footer = () => {
-  return <footer></footer>
+  return (
+    <footer className="w-full border-t">
+      <div className="wrapper flex min-h-64 flex-col items-center justify-around">
+        <Link href="/">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/assets/images/logo.svg"
+              width={36}
+              height={36}
+              alt="ODS Logo"
+              className="mb-2"
+            />
+            <span className="text-lg font-semibold">ODS Study</span>
+          </div>
+        </Link>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
