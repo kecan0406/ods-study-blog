d4d9f7c8955983111c020a196d92ada34404c51b
kecan0406 <kecan0406@gmail.com>
2024-03-10 01:58:04 +0900

    ✨ 블로그 뼈대 작성

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,3 +1,44 @@
-export default function BlogPage() {
-  return <></>
+import { Article, fetchArticles } from '@/api/blog'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import Link from 'next/link'
+
+async function getData(): Promise<Article[]> {
+  return await fetchArticles()
+}
+
+export default async function BlogPage() {
+  const articles = await getData()
+
+  return (
+    <div className="wrapper p-16">
+      <div className="grid grid-cols-1 gap-4 p-2">
+        {articles.map(article => {
+          return (
+            <Link href={`/blog/${article.name}`} key={article.name}>
+              <Card className="min-h-full min-w-full transition-colors hover:border-gray-400">
+                <CardHeader className="relative p-4">
+                  <time
+                    className="text-xs text-muted-foreground"
+                    dateTime={article.releaseDate}
+                  >
+                    {article.releaseDate}
+                  </time>
+                  <CardTitle className="text-lg">{article.title}</CardTitle>
+                  <CardDescription className="h-16 overflow-hidden">
+                    {article.description}
+                  </CardDescription>
+                  <div className="pointer-events-none absolute bottom-4 right-4 block h-6 w-3/5 bg-gradient-to-r from-transparent to-background" />
+                </CardHeader>
+              </Card>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
