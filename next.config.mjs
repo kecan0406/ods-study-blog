aa72cb28c3b30ea1de5362f716fa10ca054f0c41
kecan0406 <kecan0406@gmail.com>
2024-04-04 08:00:23 +0900

    ➖ 필요없는 종속성 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,10 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
         pathname: '/*'
-      }
+      } 
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048]
   }
 }
 
-export default bundleAnalyzer(nextConfig)
+export default nextConfig
