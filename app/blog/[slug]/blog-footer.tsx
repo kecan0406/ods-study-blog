29850bae4361290365227b2bfdc6017ee685f42f
kecan0406 <kecan0406@gmail.com>
2024-04-11 14:52:41 +0900

    ✨ 블로그 이전글 다음글 추가

---
+++ app/blog/[slug]/blog-footer.tsx
@@ -0,0 +1,37 @@
+import { Article, fetchArticles } from '@/utils/api/blog'
+import Link from 'next/link'
+
+const getArticles = async (): Promise<Article[]> => {
+  const articles = await fetchArticles()
+  return articles.toSorted(
+    (a, b) => new Date(b.matter.releaseDate).getTime() - new Date(a.matter.releaseDate).getTime()
+  )
+}
+
+export default async function BlogFooter({ slug }: { slug: string }) {
+  const articles = await getArticles()
+  const curIdx = articles.findIndex((article) => article.matter.slug === slug)
+  const prev = articles[curIdx - 1]
+  const next = articles[curIdx + 1]
+
+  return (
+    <div className='grid grid-cols-2 text-center not-prose'>
+      <div>
+        <p className='font-bold'>이전 글</p>
+        {prev && (
+          <Link className='p-3 opacity-50 hover:opacity-100' href={`/blog/${prev.matter.slug}`}>
+            {prev.matter.title}
+          </Link>
+        )}
+      </div>
+      <div>
+        <p className='font-bold'>다음 글</p>
+        {next && (
+          <Link className='p-3 opacity-50 hover:opacity-100' href={`/blog/${next.matter.slug}`}>
+            {next.matter.title}
+          </Link>
+        )}
+      </div>
+    </div>
+  )
+}
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import BlogFooter from '@/app/blog/[slug]/blog-footer'
 import BlogHeader from '@/app/blog/[slug]/blog-header'
 import ArticleMdxRemote from '@/app/components/mdx'
 import { Separator } from '@/app/components/ui/separator'
@@ -6,12 +7,13 @@ import { mdxRemoteOptions } from '@/utils/md-utils'
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { matter, content } = await fetchArticle(params.slug)
-
   return (
     <article className='wrapper py-8 prose prose-zinc md:prose-lg dark:prose-invert prose-figcaption:mt-0'>
       <BlogHeader matter={matter} />
       <Separator className='my-4' />
       <ArticleMdxRemote options={mdxRemoteOptions} source={content} />
+      <Separator className='my-4' />
+      <BlogFooter slug={matter.slug} />
     </article>
   )
 }
