a8de72fa97fd23878f55ce4522ec1e3e644a1658
kecan0406 <kecan0406@gmail.com>
2024-04-14 12:27:01 +0900

    🍻 Flashlight

--- app/components/theme-toggle-button.tsx
+++ app/components/theme-toggle-button.tsx
@@ -2,30 +2,56 @@
 
 import { Button } from 'app/components/ui/button'
 import { useTheme } from 'next-themes'
-import { MdCircle, MdDarkMode, MdLightMode } from 'react-icons/md'
+import React, { useEffect, useRef } from 'react'
+import { MdDarkMode, MdLightMode } from 'react-icons/md'
 import { useMounted } from 'utils/hooks/use-mounted'
 
 export default function ThemeToggleButton() {
   const { resolvedTheme, setTheme } = useTheme()
-  const mounted = useMounted()
+  const flashlightRef = useRef<HTMLDivElement>(null)
+  const countRef = useRef(0)
+  const handleTheme = (e: React.MouseEvent) => {
+    countRef.current++
+    handleFlashlight(e.nativeEvent)
 
+    const theme = resolvedTheme === 'dark' ? 'light' : 'dark'
+    setTheme(countRef.current >= 10 ? 'light' : theme)
+  }
+
+  const handleFlashlight = (e: MouseEvent) => {
+    const count = countRef.current
+    if (count >= 10) {
+      const flashlight = flashlightRef.current!
+      flashlight.style.setProperty('display', 'block')
+      flashlight.style.setProperty('--Xpos', `${e.clientX}px`)
+      flashlight.style.setProperty('--Ypos', `${e.clientY}px`)
+
+      if (count >= 11) {
+        flashlight.style.setProperty('display', 'none')
+        countRef.current = 0
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleFlashlight)
+    return () => {
+      window.removeEventListener('mousemove', handleFlashlight)
+    }
+  }, [])
+
+  const mounted = useMounted()
   if (!mounted) {
-    return (
-      <Button variant='ghost' size='icon' className='h-10 w-10 rounded-full'>
-        <MdCircle className='h-6 w-6' />
-      </Button>
-    )
+    return null
   }
 
   return (
-    <Button
-      variant='ghost'
-      size='icon'
-      className='h-10 w-10 rounded-full'
-      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-    >
-      {resolvedTheme === 'dark' && <MdDarkMode className='h-6 w-6' />}
-      {resolvedTheme === 'light' && <MdLightMode className='h-6 w-6' />}
-    </Button>
+    <>
+      <Button variant='ghost' size='icon' className='h-10 w-10 rounded-full' onClick={handleTheme}>
+        {resolvedTheme === 'dark' && <MdDarkMode className='h-6 w-6' />}
+        {resolvedTheme === 'light' && <MdLightMode className='h-6 w-6' />}
+      </Button>
+      <div id='flashlight' ref={flashlightRef} />
+    </>
   )
 }
