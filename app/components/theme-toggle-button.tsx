018034a9e6f9ec43ee4c64ea8eb6559e6547eac3
kecan0406 <kecan0406@gmail.com>
2024-04-18 11:43:08 +0900

    ⚡️ Flashligh 최적화

--- app/components/theme-toggle-button.tsx
+++ app/components/theme-toggle-button.tsx
@@ -2,78 +2,42 @@
 
 import { Button } from 'app/components/ui/button'
 import { useTheme } from 'next-themes'
-import React, { useEffect, useRef } from 'react'
-import { MdDarkMode, MdLightMode } from 'react-icons/md'
+import { useState } from 'react'
+import { MdDarkMode, MdFlashlightOff, MdFlashlightOn, MdLightMode } from 'react-icons/md'
+import { useFlashlight } from 'utils/hooks/use-flashlight'
 import { useMounted } from 'utils/hooks/use-mounted'
 
 export default function ThemeToggleButton() {
   const { resolvedTheme, setTheme } = useTheme()
-  const flashlightRef = useRef<HTMLDivElement>(null)
-  const countRef = useRef(0)
-  const positionRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 })
+  const [count, setCount] = useState<number>(0)
 
-  useEffect(() => {
-    window.addEventListener('mousemove', handlePosition)
-    window.addEventListener('touchmove', handlePosition)
-    return () => {
-      window.removeEventListener('mousemove', handlePosition)
-      window.removeEventListener('touchmove', handlePosition)
-    }
-  }, [])
+  const isPrevFL = count >= 9
+  const isFL = count >= 10
 
-  const handleTheme = (e: React.MouseEvent) => {
-    countRef.current++
-    handleFL()
-
+  const handleTheme = () => {
     const theme = resolvedTheme === 'dark' ? 'light' : 'dark'
-    if (countRef.current >= 10) {
-      handlePosition(e.nativeEvent)
-      setTheme('light')
-    } else {
-      setTheme(theme)
-    }
+    setTheme(isPrevFL ? 'light' : theme)
+    setCount(isFL ? 0 : count + 1)
   }
 
-  const handleFL = () => {
-    const count = countRef.current
-    const { x, y } = positionRef.current
-    if (count >= 10) {
-      const flashlight = flashlightRef.current!
-      flashlight.style.setProperty('display', 'block')
-      flashlight.style.setProperty('--Xpos', `${x}px`)
-      flashlight.style.setProperty('--Ypos', `${y}px`)
-
-      if (count >= 11) {
-        flashlight.style.setProperty('display', 'none')
-        countRef.current = 0
-      }
-    }
-  }
-
-  const handlePosition = (e: MouseEvent | TouchEvent) => {
-    if (e.type === 'mousemove') {
-      const event = e as MouseEvent
-      positionRef.current = { x: event.clientX, y: event.clientY }
-    }
-    if (e.type === 'touchmove') {
-      const event = e as TouchEvent
-      positionRef.current = { x: event.touches[0].clientX, y: event.touches[0].clientY }
-    }
-    handleFL()
-  }
-
-  const mounted = useMounted()
-  if (!mounted) {
-    return null
-  }
-
+  const flashlightRef = useFlashlight(isFL)
+  if (!useMounted()) return null
   return (
     <>
       <Button variant='ghost' size='icon' className='h-10 w-10 rounded-full' onClick={handleTheme}>
-        {resolvedTheme === 'dark' && <MdDarkMode className='h-6 w-6' />}
-        {resolvedTheme === 'light' && <MdLightMode className='h-6 w-6' />}
+        {isPrevFL ? (
+          isFL ? (
+            <MdFlashlightOff className='theme-icon' />
+          ) : (
+            <MdFlashlightOn className='theme-icon' />
+          )
+        ) : resolvedTheme === 'dark' ? (
+          <MdDarkMode className='theme-icon' />
+        ) : (
+          <MdLightMode className='theme-icon' />
+        )}
       </Button>
-      <div id='flashlight' ref={flashlightRef} />
+      <div id='flashlight' className={isFL ? 'block' : 'hidden'} ref={flashlightRef} />
     </>
   )
 }
---
+++ utils/hooks/use-flashlight.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { RefObject, useEffect, useRef } from 'react'
+
+export const useFlashlight = (isFL: boolean): RefObject<HTMLDivElement> => {
+  const flashlightRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!isFL) return
+    const flashlight = flashlightRef.current!
+
+    const mouseMove = (e: MouseEvent) => lightMove(e.clientX, e.clientY)
+    const touchMove = (e: TouchEvent) => lightMove(e.touches[0].clientX, e.touches[0].clientY)
+
+    const lightMove = (x: number, y: number) => {
+      flashlight.style.setProperty('--Xpos', `${x}px`)
+      flashlight.style.setProperty('--Ypos', `${y}px`)
+    }
+
+    const { x, y } = flashlight.getBoundingClientRect()
+    lightMove(x, y)
+
+    window.addEventListener('mousemove', mouseMove)
+    window.addEventListener('touchmove', touchMove)
+    return () => {
+      window.removeEventListener('mousemove', mouseMove)
+      window.removeEventListener('touchmove', touchMove)
+    }
+  }, [isFL])
+
+  return flashlightRef
+}
