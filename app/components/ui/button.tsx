95593c64c427c694471c3b0ab8408c3b305ce280
kecan0406 <kecan0406@gmail.com>
2024-06-14 06:55:50 +0900

    ♻️ ForwardRef 제거

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,6 +1,6 @@
 import { Slot } from '@radix-ui/react-slot'
 import { type VariantProps, cva } from 'class-variance-authority'
-import * as React from 'react'
+import { ButtonHTMLAttributes, RefObject } from 'react'
 import { cn } from 'utils/utils'
 
 const buttonVariants = cva(
@@ -29,18 +29,15 @@ const buttonVariants = cva(
   }
 )
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  ref?: RefObject<HTMLButtonElement | null>
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button'
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
-  }
-)
+const Button = ({ ref, className, variant, size, asChild = false, ...props }: ButtonProps) => {
+  const Comp = asChild ? Slot : 'button'
+  return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+}
 Button.displayName = 'Button'
 
 export { Button, buttonVariants }
--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,48 +1,63 @@
 import { Slot } from '@radix-ui/react-slot'
-import * as React from 'react'
+import { HTMLAttributes, RefObject } from 'react'
 import { cn } from 'utils/utils'
 
-export interface CardProps extends React.HTMLAttributes<HTMLElement> {
+export interface CardProps extends HTMLAttributes<HTMLElement> {
   asChild?: boolean
   as?: string
+  ref?: RefObject<HTMLElement>
 }
 
-const Card = React.forwardRef<HTMLElement, CardProps>(({ className, asChild, as, ...props }, ref) => {
+const Card = ({ ref, className, asChild, as, ...props }: CardProps) => {
   const Comp = asChild ? Slot : as ?? 'div'
   return (
     <Comp ref={ref} className={cn('rounded-xl border bg-card text-card-foreground shadow', className)} {...props} />
   )
-})
+}
 Card.displayName = 'Card'
 
-const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('flex flex-col space-y-1.5 p-2', className)} {...props} />
-  )
-)
+const CardHeader = ({
+  ref,
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement> & {
+  ref?: RefObject<HTMLDivElement>
+}) => <div ref={ref} className={cn('flex flex-col space-y-1.5 p-2', className)} {...props} />
 CardHeader.displayName = 'CardHeader'
 
-const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
-  ({ className, ...props }, ref) => (
-    <h3 ref={ref} className={cn('font-semibold leading-none tracking-tight', className)} {...props} />
-  )
-)
+const CardTitle = ({
+  ref,
+  className,
+  ...props
+}: HTMLAttributes<HTMLHeadingElement> & {
+  ref?: RefObject<HTMLHeadingElement>
+}) => <h3 ref={ref} className={cn('font-semibold leading-none tracking-tight', className)} {...props} />
 CardTitle.displayName = 'CardTitle'
 
-const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
-  ({ className, ...props }, ref) => (
-    <p ref={ref} className={cn('text-muted-foreground text-sm', className)} {...props} />
-  )
-)
+const CardDescription = ({
+  ref,
+  className,
+  ...props
+}: HTMLAttributes<HTMLParagraphElement> & {
+  ref?: RefObject<HTMLParagraphElement>
+}) => <p ref={ref} className={cn('text-muted-foreground text-sm', className)} {...props} />
 CardDescription.displayName = 'CardDescription'
 
-const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
-)
+const CardContent = ({
+  ref,
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement> & {
+  ref?: RefObject<HTMLDivElement>
+}) => <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
 CardContent.displayName = 'CardContent'
 
-const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn('flex items-center p-6 pt-0', className)} {...props} />
+const CardFooter = ({
+  ref,
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement> & { ref?: RefObject<HTMLDivElement> }) => (
+  <div ref={ref} className={cn('flex items-center p-6 pt-0', className)} {...props} />
 )
 CardFooter.displayName = 'CardFooter'
 
