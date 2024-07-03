63921599a8a1520faba3f1e5089c237d68d7d434
kecan0406 <kecan0406@gmail.com>
2024-07-03 21:38:32 +0900

    🐛 Menu drawer disable scroll

--- app/components/menu-drawer.tsx
+++ app/components/menu-drawer.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import { clsx } from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -16,7 +17,13 @@ export default function MenuDrawer() {
 
   return (
     <>
-      <Button variant='ghost' className='rounded-full md:hidden' size='icon' popoverTarget='menu-drawer'>
+      <Button
+        variant='ghost'
+        className='rounded-full md:hidden'
+        size='icon'
+        popoverTarget='menu-drawer'
+        onClick={() => disableBodyScroll(document.body)}
+      >
         <MdFormatListBulleted className='size-6' />
       </Button>
       <div
@@ -24,28 +31,42 @@ export default function MenuDrawer() {
         popover=''
         id='menu-drawer'
         ref={(ref) => {
-          ref && ref.hidePopover()
+          return () => {
+            ref?.hidePopover()
+            enableBodyScroll(document.body)
+          }
         }}
       >
         <header className='wrapper absolute top-0 left-0 flex h-12 items-center justify-end'>
-          <Button variant='ghost' className='rounded-full' size='icon' popoverTarget='menu-drawer'>
+          <Button
+            variant='ghost'
+            className='rounded-full'
+            size='icon'
+            popoverTarget='menu-drawer'
+            onClick={() => enableBodyScroll(document.body)}
+          >
             <MdFormatListBulleted className='size-6' />
           </Button>
         </header>
         <nav className='size-full text-2xl'>
           <ul className='flex h-full flex-col items-center justify-center space-y-3'>
-            {LINKS.map(({ href, text }) => (
-              <li key={href}>
-                <Link
-                  className={clsx('h-9 px-4 py-2 opacity-50 transition-opacity hover:opacity-100', {
-                    'font-bold opacity-100': href === (anchor.startsWith('@') ? 'writer' : anchor)
-                  })}
-                  href={`/${href}`}
-                >
-                  {text}
-                </Link>
-              </li>
-            ))}
+            {LINKS.map(({ href, text }) => {
+              const isActive = href === (anchor.startsWith('@') ? 'writer' : anchor)
+
+              return (
+                <li key={href}>
+                  <Link
+                    className={clsx(
+                      'h-9 px-4 py-2 transition-opacity hover:opacity-100',
+                      isActive ? 'font-bold opacity-100' : 'opacity-50'
+                    )}
+                    href={`/${href}`}
+                  >
+                    {text}
+                  </Link>
+                </li>
+              )
+            })}
           </ul>
         </nav>
       </div>
